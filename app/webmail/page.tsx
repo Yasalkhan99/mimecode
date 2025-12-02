5f23ead36d5efbadca805520c3834d0b7cf1744d
unknown <yasalkhan90@gmail.com>
2025-12-03 00:41:39 +0500

    Update webmail to port 2096

--- app/webmail/page.tsx
+++ app/webmail/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 
 // Webmail URL - Change this to your actual webmail URL
-const WEBMAIL_URL = 'https://mimecode.com:2083';
+const WEBMAIL_URL = 'https://mimecode.com:2096';
 
 export default function WebmailRedirect() {
   // Server-side redirect
